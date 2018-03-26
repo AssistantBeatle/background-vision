5d7bfb5cd69f789d1e4e1fead72e173f76066b9e
lakshyaarora <lakshya8hpdhingra@gmail.com>
2018-03-27 00:52:50 +0530

    Add files via upload

---
+++ Commander/commander.py
@@ -0,0 +1,22 @@
+from Word import wordmain
+from Mousehover import mousehover
+from Speechmanager.TTS import tts
+from Browser import ChromeMain
+class commander:
+    @staticmethod
+    def main(command):
+        print(command)
+        if(command == "open msword"):
+            wordmain.main()
+        if(command == "start mousehover"):
+            mousehover.main()
+        if(command == "stop mousehover"):
+            mousehover.stop_mousehover()
+        if(command == "open chrome"):
+            ChromeMain.main()
+        if(command == "exit"):
+
+            return -1
+        else:
+            tts.say('error')
+            print('error')
