8d1aa279049193d4e2670670fa8060aa405c56a8
lakshyaarora <lakshya8hpdhingra@gmail.com>
2018-03-28 18:35:46 +0530

    Update speech_manager.py

--- Speechmanager/speech_manager.py
+++ Speechmanager/speech_manager.py
@@ -15,10 +15,13 @@ class speech:
                 tts.say('give your command')
                 r.adjust_for_ambient_noise(source)
                 audio = r.listen(source)
+                if(audio):
                 # for testing purposes, we're just using the default API key
                 # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
                 # instead of `r.recognize_google(audio)`
-                return r.recognize_google(audio)
+                   return r.recognize_google(audio)
+                else:
+                   return NULL
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
